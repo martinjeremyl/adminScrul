c275120151aba8b1d53134f2c6702228dfafb4b9
jeremy <jeremymartinsio1@gmail.com>
2018-07-20 15:19:41 +0200

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 import React from 'react';
-import { HydraAdmin } from '@api-platform/admin';
+import parseHydraDocumentation from '@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation';
+import { HydraAdmin, hydraClient, fetchHydra as baseFetchHydra } from '@api-platform/admin';
+import authProvider from './authProvider';
+import { Redirect } from 'react-router-dom';
 
-//export default () => <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/>;
-export default () => <HydraAdmin entrypoint='https://vps507792.ovh.net:8443'/>;
+const entrypoint = 'http://vps507792.ovh.net:8080/login_check';
+const fetchHeaders = {'Authorization': `Bearer ${window.localStorage.getItem('token')}`};
+const fetchHydra = (url, options = {}) => baseFetchHydra(url, {
+    ...options,
+    headers: new Headers(fetchHeaders),
+});
+const dataProvider = api => hydraClient(api, fetchHydra);
+const apiDocumentationParser = entrypoint => parseHydraDocumentation(entrypoint, { headers: new Headers(fetchHeaders) })
+    .then(
+        ({ api }) => ({ api }),
+        (result) => {
+            switch (result.status) {
+                case 401:
+                    return Promise.resolve({
+                        api: result.api,
+                        customRoutes: [{
+                            props: {
+                                path: '/',
+                                render: () => <Redirect to={`/login`}/>,
+                            },
+                        }],
+                    });
+
+                default:
+                    return Promise.reject(result);
+            }
+        },
+    );
+
+export default props => (
+    <HydraAdmin
+        apiDocumentationParser={apiDocumentationParser}
+        authProvider={authProvider}
+        entrypoint={entrypoint}
+        dataProvider={dataProvider}
+    />
+);
---
+++ src/authProvider.js
@@ -0,0 +1,44 @@
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
+
+const login_uri = 'http://vps507792.ovh.net:8080/login_check';
+
+export default (type, params) => {
+  switch (type) {
+    case AUTH_LOGIN:
+      const { username, password } = params;
+      const request = new Request(`${login_uri}`, {
+        method: 'POST',
+        body: JSON.stringify({ email: username, password }),
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+      });
+
+      return fetch(request)
+        .then(response => {
+          if (response.status < 200 || response.status >= 300) throw new Error(response.statusText);
+
+          return response.json();
+        })
+        .then(({ token }) => {
+          localStorage.setItem('token', token); // The JWT token is stored in the browser's local storage
+          window.location.replace('/');
+        });
+
+    case AUTH_LOGOUT:
+      localStorage.removeItem('token');
+      break;
+
+    case AUTH_ERROR:
+      if (401 === params.status || 403 === params.status) {
+        localStorage.removeItem('token');
+
+        return Promise.reject();
+      }
+      break;
+
+    case AUTH_CHECK:
+      return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
+
+      default:
+          return Promise.resolve();
+  }
+}
